939b835cc79bdab9a53d9df7d93c11181d0e6ade
Luis Ampuero <luis.ampuero@ug.uchile.cl>
2018-04-27 16:13:31 -0300

    Creando modelos de reserva y prestamo

--- Inventario_CEI/Inventario_CEI/settings.py
+++ Inventario_CEI/Inventario_CEI/settings.py
@@ -31,13 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'articles.apps.ArticlesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # internal apps
+    #'articles.apps.ArticlesConfig',
+    'reservations',
 ]
 
 MIDDLEWARE = [
---
+++ reservations/models.py
@@ -0,0 +1,70 @@
+# Django
+from django.db import models
+import datetime
+# Models
+
+# Enum
+from .enums import *
+# Create your models here.
+
+
+class Reservation(models.Model):
+    """
+    Modelo para reservas.
+    """
+    # ID(generado por  django)
+    # RUT[foreign key ref Usuario]
+    #user = models.ForeignKey()
+    # Espacio[foreign key  ref Espacio]
+    #article_or_space = models.ForeignKey()
+    # Fecha Inicio[Datetime]
+    initial_date = models.DateTimeField(
+        verbose_name="initial date",
+        default=datetime.datetime.now()
+    )
+    # Fecha Término[Datetime]
+    end_date = models.DateTimeField(
+        verbose_name="end date",
+        default=datetime.datetime.now()
+    )
+    state = models.IntegerField(
+        choices=ReservationEnum.RESERVATION_STATES,
+        default=ReservationEnum.PENDIENTE
+    )
+
+    def __str__(self):
+        return "{} {} {} {}".format(
+            "user",#self.user,
+            "article",#self.article_or_space,
+            self.initial_date,
+            self.end_date
+        )
+
+
+class Loan(models.Model):
+    """
+    Modelo para prestamos.
+    """
+    # ID(generado por  django)
+    # user [foreign key ref Usuario]
+    #user = models.ForeignKey()
+    # articulo o Espacio [foreign key  ref Espacio]
+    #article_or_space = models.ForeignKey()
+    # Fecha Inicio[Datetime]
+    initial_date = models.DateTimeField(
+        verbose_name="initial date",
+        default=datetime.datetime.now()
+    )
+    # Fecha Término[Datetime]
+    end_date = models.DateTimeField(
+        verbose_name="end date",
+        default=datetime.datetime.now()
+    )
+
+    def __str__(self):
+        return "{} {} {} {}".format(
+            "user",#self.user,
+            "article",#self.article_or_space,
+            self.initial_date,
+            self.end_date
+        )
