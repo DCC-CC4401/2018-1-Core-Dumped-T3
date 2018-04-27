1f3768fc5d0ec89ef5641700710a81e2a31a1fc0
Luis Ampuero <luis.ampuero@ug.uchile.cl>
2018-04-27 16:52:24 -0300

    Corregido mala configuracion carpetas proyecto django
    Se cometio el error de crear un proyecto Django y luego agregarlo como una carpeta mas al repositorio.

--- Inventario_CEI/settings.py
+++ Inventario_CEI/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # internal apps
-    #'articles.apps.ArticlesConfig',
+    'articles',
     'reservations',
 ]
 
