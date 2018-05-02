72fbdcd7e68e8013d37cad605e59073ec9d03129
danno-s <danielsoto.3004@gmail.com>
2018-05-02 19:23:33 -0300

    agrega bootstrap al proyecto y mejora implementacion de estado de un articulo

--- Inventario_CEI/articles/models.py
+++ Inventario_CEI/articles/models.py
@@ -1,19 +1,31 @@
 from django.db import models
 
+from .enums import ArticleEnum
+
 # Create your models here.
 class Article(models.Model):
+    DISPONIBLE = 0
+    PRESTAMO = 1
+    REPARACION = 2
+    PERDIDO = 3
+
+    ARTICLE_STATES =  (
+        (DISPONIBLE, "Disponible"),
+        (PRESTAMO, "En préstamo"),
+        (REPARACION, "En reparación"),
+        (PERDIDO, "Perdido"),
+    )
+    
     name = models.CharField(max_length=128) # muy arbitrario
     image = models.BinaryField()
     description = models.TextField()
-    status = models.PositiveSmallIntegerField(default=0)
+    status = models.PositiveSmallIntegerField(
+        choices=ARTICLE_STATES,
+        default=DISPONIBLE
+    )
 
     def pretty_status(self):
-        return {
-            0: "Disponible",
-            1: "En préstamo",
-            2: "En reparación",
-            3: "Perdido",
-        }.get(self.status, "Desconocido")
+        return self.ARTICLE_STATES[self.status][1]
 
     def __str__(self):
         return self.name
