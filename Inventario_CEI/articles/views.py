215439b2a730c1ab9cd2907d563140d40a764284
danno-s <danielsoto.3004@gmail.com>
2018-05-08 18:20:38 -0300

    tempus dominos time picker added

--- Inventario_CEI/articles/views.py
+++ Inventario_CEI/articles/views.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 
 from .models import Article
 from reservations.models import ArticleReservation
+from reservations.forms import ArticleReservationForm
 
 # Create your views here.
 @login_required(login_url="/users/login/")
@@ -11,6 +12,6 @@ def detail(request, article_id):
     article = get_object_or_404(Article, id=article_id)
     reservations = ArticleReservation.objects.filter(article=article, initial_date__gte=timezone.now())
 
-    
+    form = ArticleReservationForm()
 
-    return render(request, 'articles/detail.html', {'article': article, 'reservations': reservations})
+    return render(request, 'articles/detail.html', {'article': article, 'reservations': reservations, 'form': form})
