2fec39d0027e085265cbc3fdc55799a2487bdc1c
lampuero <luis.ampuero@ug.uchile.cl>
2018-07-04 15:38:11 -0400

    Agregada url de los botones

--- Inventario_CEI/reservations/urls.py
+++ Inventario_CEI/reservations/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from django.conf import settings
 
-from . import views
+from .views import ReservationsView
+from .views import change_loan_to_received
+from .views import change_loan_to_lost
+from .views import accept_reservation
+from .views import reject_reservation
 
 urlpatterns = [
-    path('', views.ReservationsView.as_view(), name='reservations'),
+    path('', ReservationsView.as_view(), name='reservations'),
+    path('/loan-received', change_loan_to_received, name='loan-received'),
+    path('/loan-lost', change_loan_to_lost, name='loan-lost'),
+    path('/reservation-accepted', accept_reservation, name='reservation-accepted'),
+    path('/reservation-rejected', reject_reservation, name='reservation-rejected'),
 ]
