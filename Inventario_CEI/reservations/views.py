56047075e1b9420dc2446e069b2fe82f3100a74c
lampuero <luis.ampuero@ug.uchile.cl>
2018-07-04 16:53:39 -0400

    Agregada respuesta a boton prestamos perdidos

--- Inventario_CEI/reservations/views.py
+++ Inventario_CEI/reservations/views.py
@@ -42,7 +42,13 @@ def reject_reservation(request):
 
 
 def change_loan_to_lost(request):
-    pass
+    if request.method == 'POST':
+        loans = request.POST.getlist('loans')
+        for loan_id in loans:
+            loan = Loan.objects.get(id=loan_id)
+            loan.state = Loan.PERDIDO
+            loan.save()
+    return redirect('reservations')
 
 
 def change_loan_to_received(request):
